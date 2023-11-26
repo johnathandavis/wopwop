28b93faea347e9b8887621162e33d563be864cf8
John Davis <john@johndavis.dev>
2023-11-26 20:58:13 +1300

    Initial commit.

---
+++ Docs/README.md
@@ -0,0 +1,13 @@
+```mermaid
+flowchart LR
+    head --> hair([long green hair])
+    head(( )) --> torso([beautiful torso])
+    torso -- Arm --> leftArm(( ))
+    torso -- spine --> tail
+    torso -- Arm --> rightArm(( ))
+    tail -- fin --> leftTip(( ))
+    tail --> tip(( ))
+    tail -- fin --> rightTip(( ))
+    leftTip --> leftFin(( ))
+    rightTip --> rightFin(( ))
+```
---
+++ Examples/LocalExecution/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace LocalExecution;
+
+public class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the delay show!");
+        await Task.Delay(1000);
+        
+        if (args.Length % 2 == 0)
+        {
+            Console.WriteLine("Double delay penalty.");
+            await Task.Delay(2000);
+        }
+        else
+        {
+            Console.WriteLine("No delay for you.");
+        }
+
+        return await DelayWithRandomResultAsync();
+    }
+
+    private static async Task<int> DelayWithRandomResultAsync()
+    {
+        Random rnd = new Random();
+        await Task.Delay(1000);
+        return rnd.Next();
+    }
+}
