2209a7ddadb46a6c50f5a3f86b670934d37e8a81
John Davis <john@johndavis.dev>
2023-11-26 22:23:15 +1300

    Incremented.

--- Packages/Analysis/WopWop.Analysis.Structure/publish.sh
+++ Packages/Analysis/WopWop.Analysis.Structure/publish.sh
@@ -5,5 +5,5 @@ dotnet build -c Release
 dotnet pack
 PKG=$(ls bin/Release/WopWop.Analysis.Structure*)
 echo "Pushing package $PKG..."
-nuget push $PKG -Source https://api.nuget.org/v3/index.json
+nuget push $PKG -Source https://api.nuget.org/v3/index.json -NoServiceEndpoint
 
